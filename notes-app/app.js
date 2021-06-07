f56f2bf052a54f9738b3351f74385701a259e5a9
Ylva-Thorsen <ylva.e.thorsen@gmail.com>
2021-06-07 14:19:27 +0100

    Install yargs and create a few commands

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,46 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes');
 
-const message = getNotes();
-console.log(message);
+// Customise yargs version
+yargs.version('1.1.0');
 
-const chalkMsg = chalk.blue.inverse('Error!');
-console.log(chalkMsg);
+// add, remove, read, list
+
+// create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler() {
+    console.log('Adding a new note');
+  }
+});
+
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler() {
+    console.log('Removing the note');
+  }
+});
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List your notes',
+  handler() {
+    console.log('Listing notes');
+  }
+});
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler() {
+    console.log('Reading a note');
+  }
+});
+
+console.log(yargs.argv);

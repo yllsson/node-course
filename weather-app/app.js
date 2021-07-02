5f9c6d8377769cb9bcdfe3fc0290f29d363f6941
Ylva-Thorsen <ylva.e.thorsen@gmail.com>
2021-07-02 18:48:38 +0100

    add geocoding API and make new request

--- weather-app/app.js
+++ weather-app/app.js
@@ -14,3 +14,15 @@ request({ url: url, json: true }, (error, response) => {
     `${weather_description}. It is currently ${temperature} degrees out. It feels like ${feelsLike} degrees. There is a ${precip}% chance of rain.`
   );
 });
+
+// Geocoding
+
+const geoURL =
+  'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoieWx2YXR1cm5lciIsImEiOiJja3FtbTJ4a3IwMTFtMm9ueGE5eGFoOXNmIn0.MFgz-eXa73qyMR-43lIleg&limit=1';
+
+request({ url: geoURL, json: true }, (error, response) => {
+  const data = response.body.features;
+  const latitude = data[0].center[1];
+  const longitude = data[0].center[0];
+  console.log(latitude, longitude);
+});

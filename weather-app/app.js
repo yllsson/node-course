c597e15d9988f57f9bf4acddb6e3275b96570a58
Ylva-Thorsen <ylva.e.thorsen@gmail.com>
2021-07-02 18:25:33 +0100

    fetching and manipulating more weather data

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,16 @@
 const request = require('postman-request');
 
 const url =
-  'http://api.weatherstack.com/current?access_key=dcae8625cc1f675d84f3d990a6f285bd&query=37.8267,-122.4233';
+  'http://api.weatherstack.com/current?access_key=dcae8625cc1f675d84f3d990a6f285bd&query=37.8267,-122.4233&units=f';
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+request({ url: url, json: true }, (error, response) => {
+  const data = response.body.current;
+  const temperature = data.temperature;
+  const precip = data.precip;
+  const feelsLike = data.feelslike;
+  const weather_description = data.weather_descriptions[0];
+
+  console.log(
+    `${weather_description}. It is currently ${temperature} degrees out. It feels like ${feelsLike} degrees. There is a ${precip}% chance of rain.`
+  );
 });

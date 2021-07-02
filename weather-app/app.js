a567181612adc51049cbb63851a341202c16bd22
Ylva-Thorsen <ylva.e.thorsen@gmail.com>
2021-07-02 18:03:11 +0100

    install postman-request and start exploring HTTP requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,9 @@
-console.log('Starting');
+const request = require('postman-request');
 
-setTimeout(() => {
-  console.log('2 Second Timer');
-}, 2000);
+const url =
+  'http://api.weatherstack.com/current?access_key=dcae8625cc1f675d84f3d990a6f285bd&query=37.8267,-122.4233';
 
-setTimeout(() => {
-  console.log('0 Second Timer');
-}, 0);
-
-console.log('Stopping');
+request({ url: url }, (error, response) => {
+  const data = JSON.parse(response.body);
+  console.log(data.current);
+});

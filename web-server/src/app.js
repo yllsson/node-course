485ff4c66e6929ea02615d8fb7b6b8d49678f917
Ylva-Thorsen <ylva.e.thorsen@gmail.com>
2021-07-26 11:05:41 +0100

    Learning to serve HTML and JSON through Express

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,19 +4,29 @@ const app = express();
 
 // req, res = request, response
 app.get('', (req, res) => {
-  res.send('Hello express!');
+  res.send('<h1>Weather</h1>');
 });
 
 app.get('/help', (req, res) => {
-  res.send('Help page');
+  res.send([
+    {
+      name: 'Ylva'
+    },
+    {
+      name: 'Sarah'
+    }
+  ]);
 });
 
 app.get('/about', (req, res) => {
-  res.send('About page');
+  res.send('<h1>About page</h1>');
 });
 
 app.get('/weather', (req, res) => {
-  res.send('Weather page');
+  res.send({
+    forecast: 'Sunny, 20 degrees',
+    location: 'Edinburgh'
+  });
 });
 
 app.listen(3000, () => {

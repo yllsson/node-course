99dcb659ed7e813d2347baf96348e8e7430ec2df
Ylva-Thorsen <ylva.e.thorsen@gmail.com>
2021-07-29 18:52:46 +0100

    Learning about handlebars

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,12 +4,35 @@ const express = require('express');
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs');
+
 // Alternate fix to extension issue. Taken from course Q&A.
 const options = {
   extensions: ['htm', 'html']
 };
 
 app.use(express.static(publicDirectoryPath, options));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Ylva Turner'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About',
+    name: 'Ylva Turner'
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    message: 'This is the help messsage!'
+  });
+});
 
 // req, res = request, response
 app.get('/weather', (req, res) => {

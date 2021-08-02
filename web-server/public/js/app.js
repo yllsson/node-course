d68e40e6eb910b1143ee7a8df050dabd0944d6a4
Ylva-Thorsen <ylva.e.thorsen@gmail.com>
2021-08-02 14:32:40 +0100

    Creating client-side fetch request

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,16 @@
 console.log('Client side JS file is loaded!');
+
+// My solution - opted for async-await
+const myFetch = async () => {
+  const res = await fetch('http://localhost:3000/weather?address=Boston');
+  const data = await res.json();
+
+  if (data.error) {
+    console.log(data.error);
+  } else {
+    console.log(data.location);
+    console.log(data.forecast);
+  }
+};
+
+myFetch();

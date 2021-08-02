353ac43b064741791575fc5da153dfeea6e3f32e
Ylva-Thorsen <ylva.e.thorsen@gmail.com>
2021-08-02 13:59:40 +0100

    Creating a JSON HTTP endpoint

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
 const app = express();
 
@@ -51,11 +53,30 @@ app.get('/weather', (req, res) => {
     });
   }
 
-  res.send({
-    forecast: 'Sunny, 20 degrees',
-    location: 'Edinburgh',
-    address: req.query.address
-  });
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({
+          error
+        });
+      }
+
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+          return res.send({
+            error
+          });
+        }
+
+        res.send({
+          forecast: forecastData,
+          location,
+          address: req.query.address
+        });
+      });
+    }
+  );
 });
 
 app.get('/products', (req, res) => {
---
+++ web-server/src/utils/forecast.js
@@ -0,0 +1,24 @@
+const request = require('postman-request');
+
+const forecast = (latitude, longitude, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=dcae8625cc1f675d84f3d990a6f285bd&query=${encodeURIComponent(
+    latitude
+  )},${encodeURIComponent(longitude)}`;
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback('Unable to connect to Weather services!', undefined);
+    } else if (body.error) {
+      callback('Invalid location. Please try another search.', undefined);
+    } else {
+      const temperature = body.current.temperature;
+      const precip = body.current.precip;
+      const feelsLike = body.current.feelslike;
+      const weather_description = body.current.weather_descriptions[0];
+      const data = `${weather_description}. It is currently ${temperature} degrees out. It feels like ${feelsLike} degrees. There is a ${precip}% chance of rain.`;
+      callback(undefined, data);
+    }
+  });
+};
+
+module.exports = forecast;

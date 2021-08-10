8d69cf074a4ca97256ba2a6fca0737634ccb5605
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-08-10 17:02:39 +0100

    Remove console.log from app.js

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side JS file is loaded!');
-
 const weatherForm = document.querySelector('form');
 const searchElement = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
